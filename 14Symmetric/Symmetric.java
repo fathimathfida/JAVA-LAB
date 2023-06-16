819f7253d50ace75c6fa9adc59028e27f532e9cc
fathimathfida <117343911+fathimathfida@users.noreply.github.com>
2023-06-16 15:25:10 +0530

    Add files via upload

---
+++ 14Symmetric/Symmetric.java
@@ -0,0 +1,40 @@
+    import java.util.Scanner;
+    class Symmetric
+    {
+    public static void main(String[] args)
+    {
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter the size of the square matrix:");
+    int s=sc.nextInt();
+    int[][]mat1=new int[s][s];
+    System.out.println("Enter the matrix:");
+    for(int i=0;i<s;i++)
+    {
+    for(int j=0; j<s;j++)
+    {mat1[i][j]=sc.nextInt();
+    }
+    }
+    System.out.println("Matrix:");
+    for(int i=0;i<s;i++)
+    {
+    for(int j=0;j<s;j++)
+    {
+    System.out.print(mat1[i][j]+"\t");
+    }
+    System.out.println();
+    }
+    for(int i=0; i<s;i++)
+    {
+    for(int j=0; j<s;j++)
+    {
+    if(mat1[i][j] !=mat1 [j][i])
+    {
+    System.out.println("Matrix are not symmetric:");
+    return;
+    }
+    }
+    }
+    System.out.println("Matrix are symmetric:");
+    }
+    }
+
