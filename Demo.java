dbfd15f5f9fbca98a497b9ccc571649e30edf619
fathimathfida <117343911+fathimathfida@users.noreply.github.com>
2023-06-22 12:46:59 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Demo
+{
+void multiply(int a,int b)
+{
+System.out.println("Result is:"+(a*b));
+}
+void multiply(int a,int b,int c)
+{
+System.out.println("Result is:"+(a*b*c));
+}
+public static void main(String[] args)
+{
+Demo obj=new Demo();
+obj.multiply(8,5);
+obj.multiply(4,6,2);
+}
+}
---
+++ LinkedLists.java
@@ -0,0 +1,14 @@
+import java.util.*; 
+class LinkedLists{
+public static void main(String[] args){ 
+LinkedList<String>ll=new LinkedList<String>();
+ll.add("a");
+ll.add("b");
+ll.add("c");
+ll.add("d");
+ll.add("e");
+System.out.println("current contents of ll:"+ll);
+ll.clear();
+System.out.println("contents of ll after deletion:"+ll);
+}
+}
